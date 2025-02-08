5b07162bf95c6b51121905d573d130eb5c733fa3
Marijn Schouten <hkBst@users.noreply.github.com>
2025-02-08 14:40:16 +0100

    Remove empty format precision specifier
    A format precision specifier consisting of a dot and no number actually does nothing and has no specified meaning. Currently this is silently ignored, but it may turn into a warning or error.
    
    See rust-lang/rust#131159 and rust-lang/rust#136638

--- src/main.rs
+++ src/main.rs
@@ -126,7 +126,7 @@ impl Display for Color {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(
             f,
-            "RGB ({0}, {1}, {2}) 0x{0:02.X}{1:02.X}{2:02.X}",
+            "RGB ({0}, {1}, {2}) 0x{0:02X}{1:02X}{2:02X}",
             self.red, self.green, self.blue
         )
     }
